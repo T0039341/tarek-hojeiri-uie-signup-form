31afd1f45350c178b2d056c36c4715e7f09d2f7c
t0039341 <t.hojeiri@gmail.com>
2026-05-13 14:06:00 +0100

    Finish reactive signup form implementation and add styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignupForm } from './features/signup/signup-form/signup-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SignupForm],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
